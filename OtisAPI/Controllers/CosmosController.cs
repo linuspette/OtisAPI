e9ae899a4f1eb7204c8395c0aaaf567e74691c9d
linuspette <70110168+linuspette@users.noreply.github.com>
2022-11-21 10:14:05 +0100

    Connection to cosmos db now works

---
+++ OtisAPI/Controllers/CosmosController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OtisAPI.Services;
+
+namespace OtisAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CosmosController : ControllerBase
+    {
+        private readonly ICosmosDbService _cosmosDbService;
+
+        public CosmosController(ICosmosDbService cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+
+        [HttpGet]
+        [Route("getalldata")]
+        public async Task<IActionResult> GetAllDataAsync()
+        {
+            try
+            {
+                var result = await _cosmosDbService.GetAllDataAsync();
+
+                if (result == null)
+                    return new NotFoundObjectResult("No objects found");
+
+                return new OkObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("getelevatordata")]
+        public async Task<IActionResult> GetElevatorDataAsync(string deviceId)
+        {
+            try
+            {
+                var result = await _cosmosDbService.GetElevatorDataAsync(deviceId);
+
+                if (result == null)
+                    return new NotFoundObjectResult("No objects found");
+
+                return new OkObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+    }
+}
